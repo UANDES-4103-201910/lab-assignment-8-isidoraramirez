f66041df280ec7b2b67ad75c5b1fd25638993f40
Isidora <iaramirez@miuandes.cl>
2019-05-08 07:34:30 -0400

    textadded

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,14 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	$('.key').click(function(){
+
+		var textadded = ""
+		textadded =  $('#output').text() + textadded + $(this).text();
+   		console.log($(this).text());
+   		$('#output').text(textadded) ;
+   		
+     });
+
+
 });
