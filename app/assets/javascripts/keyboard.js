778c35f241cb35e0d98a97848f35fe25ee216c24
Isidora <iaramirez@miuandes.cl>
2019-05-09 19:59:14 -0400

    adios

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,40 +1,28 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
-		//var target = '#output'
+		//var '#output' = '#output'
 		
 $(() => {
 	var i = 1;
-		var target = "#" + i;
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 	$('.key').click(function(){
 
 		var textadded = "";
 		var keytoggled = $(this).text();
-
-		if ( keytoggled === "Bksp"){
-			if ($(target).text().len() == 0 ){
-				i -= 1;
-				target = "#" + i;
-				$(target).html(textadded);
-			}
-			else{
-				target = "#" + i;
-				textadded = $(target).text().slice(0,-1);
-				$(target).html(textadded) ;
-			}
+		if ( keytoggled == "Bksp"){
+			textadded = $('#output').text().slice(0,-1);
+			$('#output').html(textadded) ;
 			
 		}
-		else if (keytoggled === "Enter"){
-			i = i + 1;
-			target = "#" + i;
-			//var textadded2 = $(target).append('</br>');
-			textadded = $(target).text() + " ";
-			//$(target).print(textadded) ;
+		else if (keytoggled == "Enter"){
+			//var textadded2 = $('#output').append('</br>');
+			textadded = $('#output').text() + ("\n");
+			//$('#output').print(textadded) ;
 			
 
 		}
 		else {
-			textadded =  $(target).text() + textadded + $(this).text();
-   			$(target).html(textadded) ;
+			textadded =  $('#output').text() + textadded + $(this).text();
+   			$('#output').html(textadded) ;
    		}
    		console.log($(this).text());
    		
